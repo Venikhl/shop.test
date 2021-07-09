18647c920a439fae9483e11b08e02fe5cb75feb7
Gleb <asd>
2021-07-09 15:35:10 +0600

    asd

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -4,13 +4,16 @@
 
     <x-partials.navbar>
 
-        <x-partials.navbar.link href="{{ url('/') }}">
-            {{__('Admin panel')}}
+        <x-partials.navbar.link href="{{ route('admin.dashboard') }}">
+            {{__('Dashboard')}}
         </x-partials.navbar.link>
 
     </x-partials.navbar>
 
     <div class="container mt-3">
+
+        <h1>{{ $title }}</h1>
+
         {{ $slot }}
     </div>
 </x-layouts.base>
--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,10 @@
 <?php
 
-use \App\Http\Controllers\Admin;
-use \Illuminate\Support\Facades;
+use \App\Http\Controllers\Admin\DashboardController;
+use \App\Http\Controllers\Admin\CategoryController;
+use \Illuminate\Support\Facades\Route;
+
+Route::get('/', DashboardController::class)
+    ->name('dashboard');
 
 Route::resource('categories', CategoryController::class);
